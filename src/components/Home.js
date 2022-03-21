43d8291f6c78b9c5f1f1910728de466d8942565c
AmalAlmutair <asmutairi86@gmail.com>
2022-03-21 20:17:06 +0300

    add signin and sign out frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import styles from "./styles";
+import authStore from "../stores/authStore";
+import { Button, NativeBaseProvider } from "native-base";
 
-const Home = ({ navigation }) => {
+const Home = ({ route, navigation }) => {
+  const handleSignout = (user) => {
+    authStore.signout(user);
+    navigation.navigate("Signin");
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title} onPress={() => navigation.navigate("Signup")}>
+      <NativeBaseProvider>
+        <Button
+          color="rgba(99, 32, 238, 1)"
+          onPress={handleSignout}
+          title="SignOut"
+        >
+          SignOut
+        </Button>
+      </NativeBaseProvider>
+      <Text style={styles.title} onPress={() => navigation.navigate("Signin")}>
         Hello
       </Text>
     </View>
